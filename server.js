d29f1babedb937c2a86c9fecbc8b3b8981bd308e
Unknown <k.ballagher@gmail.com>
2019-01-02 13:57:08 +0000

    Added CORS, Setup Passing of PostFile

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const cors = require('cors');
+
+const postFile = require('./postfile.json');
 
 const app = express();
+app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('this is working')
+    res.send("Server Online")
 })
 
-app.listen(3000, () => {console.log("Server Online")});
+
+
+const queryDb = () => (req, res) => {
+    res.send(postFile)
+}
+
+app.get('/queryDb', queryDb())
+
+var server = app.listen(process.env.PORT || 3000, () => {
+    console.log("Server Online", server.address().port);
+})
